9c2aca58fa321c2306e3b94cd3b599ed6dcf19c7
Heon Jeong <blmarket@linecorp.com>
2016-05-19 18:03:39 +0900

    Removed border from floating full-screen windows.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,30 @@
 import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.ToggleLayouts
+import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig
+import XMonad.Hooks.ManageHelpers (composeOne, isFullscreen, isDialog,  doFullFloat, doCenterFloat)
+
+myManageHook = composeAll. concat $
+    [ [ className =? c --> doCenterFloat| c <- floats]
+    , [ resource =? r --> doIgnore | r <- ignore]
+    , [ isFullscreen --> doFullFloat]
+    , [ isDialog --> doCenterFloat]]
+    where floats = ["sdlpal", "MPlayer", "Gimp", "qemu-system-x86_64", "Gnome-typing-monitor", "Vlc", "Dia", "DDMS", "Audacious", "Wine"]
+          ignore = []
+
+myLayout = tiled ||| Mirror tiled ||| noBorders Full
+  where
+      tiled   = Tall 1 (3/100) (3/5)
 
 main = do
-  xmonad $ defaultConfig
-    { terminal = "terminator"
-    , modMask = mod4Mask
-    , borderWidth = 3
-    }
-    `removeKeys` [ (mod4Mask, xK_space) ]
+    xmonad $ defaultConfig 
+      { terminal = "terminator"
+      , layoutHook = avoidStruts 
+        $ toggleLayouts (noBorders Full) 
+        $ smartBorders
+        $ myLayout
+      , modMask = mod4Mask
+      , manageHook = myManageHook <+> manageHook defaultConfig
+      } `removeKeys` [ (mod4Mask, xK_space) ]
+
