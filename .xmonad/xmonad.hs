57a644209d05288e7366b7b3ad4e1e95bebf20af
Heon Jeong <blmarket@linecorp.com>
2016-11-10 11:04:15 +0900

    Avoid some dialogs to span across multiple monitors on its creation.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,17 +1,26 @@
 import XMonad
+import qualified XMonad.StackSet as W
+import XMonad.ManageHook
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.ToggleLayouts
 import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig
 import XMonad.Hooks.ManageHelpers (composeOne, isFullscreen, isDialog,  doFullFloat, doCenterFloat)
+import XMonad.Util.NamedScratchpad
 
-myManageHook = composeAll. concat $
-    [ [ className =? c --> doCenterFloat| c <- floats]
-    , [ resource =? r --> doIgnore | r <- ignore]
-    , [ isFullscreen --> doFullFloat]
-    , [ isDialog --> doCenterFloat]]
-    where floats = ["sdlpal", "MPlayer", "Gimp", "qemu-system-x86_64", "Gnome-typing-monitor", "Vlc", "Dia", "DDMS", "Audacious", "Wine"]
-          ignore = []
+myCenterFloat = customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3)
+
+myManageHook = composeAll . concat $
+        [ [ className =? c --> doCenterFloat    | c <- floats       ]
+        , [ resource =? r --> doIgnore          | r <- ignore       ]
+        , [ role =? r --> myCenterFloat         | r <- ["GtkFileChooserDialog"]]
+        , [ isFullscreen --> doFullFloat        ]
+        , [ isDialog --> doCenterFloat          ]
+        ]
+    where
+        role = stringProperty "WM_WINDOW_ROLE"
+        floats = ["sdlpal", "MPlayer", "Gimp", "qemu-system-x86_64", "Gnome-typing-monitor", "Vlc", "Dia", "DDMS", "Audacious", "Wine"]
+        ignore = []
 
 myLayout = tiled ||| Mirror tiled ||| noBorders Full
   where
